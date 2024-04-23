04f544fc6dd61d290bbc8a0efc7a1b479c298bf3
Yasamin <yasamin.amini98@gmail.com>
2024-04-23 17:12:22 +0200

    Completed 5 parts

--- dy.js
+++ dy.js
@@ -1,2 +1,17 @@
-afdsadsfadfs
-kjlhlkh
+//1. Create a variable named myAge and set it equal to your age as a number. Write a comment that explains this line of code.
+// Variable for my age
+const myAage = 30;
+//2. Create a variable named earlyYears and save the value 2 to it. Note, the value saved to this variable will change. Write a comment that explains this line of code.
+// Variable for first two years
+
+let earlyYears = 2;
+//3. Use the multiplication assignment operator to multiply the value saved to earlyYears by 10.5 and reassign to earlyYears. 
+
+earlyYears *= 10.5;
+//4. Since we already accounted for the first two years, take the myAge variable , and subtract 2 from it. Set the result equal to a variable called laterYears. We’ll be changing this value later.  
+//takes age and subtracts 2
+let laterYears = myAage - 2;
+
+// Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by your later years. Use the multiplication assignment operator to multiply and assign in one step. 
+//Multiplying later years by 4
+laterYears *= 4;
